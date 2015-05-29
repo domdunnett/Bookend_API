c8e625f75b77a6ae42b67f6d9765838d549c541d
Dominic Michael Joshua Dunnett <domdunnett@gmail.com>
2015-05-29 15:24:25 +0800

    add mongo url to heroku

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ var plugins = [
 	{ register: require('./routes/reviews.js')},
 	{ register: require('hapi-mongodb'),
 		options: {
-			url: "mongodb://127.0.0.1/bookend",
+			url: "mongodb://heroku_app36557135:rolcc11toifn8hv2fckolairt5@ds031952.mongolab.com:31952/heroku_app36557135",
 			settings: {
 				db: {
 					native_parser: false
